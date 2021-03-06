5316c2e7338871eee3c5bc8eccf0d1bce17587a5
Chong Zheng <zhengc15@mcmaster.ca>
2021-03-06 18:00:15 -0500

    background, color generator

--- popup.js
+++ popup.js
@@ -2,7 +2,11 @@ document.addEventListener("DOMContentLoaded", () => {
   chrome.storage.local.get(null, function (obj) {
     document.getElementById("password").innerHTML = JSON.stringify(obj);
   });
-  console.log("hello world");
+  // test: display the data in Storage
+  chrome.storage.local.get(null, (value) => {
+    console.log(value)
+  });
+
 
   var ctx = document.getElementById("myChart").getContext("2d");
   // And for a doughnut chart
@@ -10,33 +14,62 @@ document.addEventListener("DOMContentLoaded", () => {
   const websiteData = [
     {
       domain: "google.com",
-      time: 2365362623,
+      time: 1000,
       limit: 325362623632622,
     },
     {
       domain: "linkedin.com",
-      time: 3532623236,
+      time: 1000,
+      limit: 353252335,
+    },
+
+    {
+      domain: "twitter.com",
+      time: 1000,
       limit: 353252335,
     },
   ];
   const websiteUrl = [];
   const websiteTime = [];
   for (let i = 0; i < websiteData.length; i++) {
-    websiteUrl[i] = websiteData[i].domain;
-    websiteTime[i] = websiteData[i].time;
+    websiteUrl[i] = websiteData[i].domain.slice(0,-4).toUpperCase();
+    websiteTime[i] = new Date(websiteData[i].time);
   }
+
+  //auto generate color
+
+  function hexCodeGen (){
+    let code = (Math.random() * 0xffff*55 ).toString();
+    return '#' + code.slice(0, 6);
+  };
+
+  function chooseColor(dataSize){
+    var i;
+    var colorArray = [];
+    for (i = 0; i < dataSize; i++) {
+      colorArray.push(hexCodeGen());
+    }
+
+    return colorArray;
+  }
+
+  var colors = chooseColor(websiteData.length)
   const data = {
     datasets: [
       {
         data: websiteTime,
-      },
+        backgroundColor: colors,
+      }
     ],
     // These labels appear in the legend and in the tooltips when hovering different arcs
     labels: websiteUrl,
-    color: ["red", "green", "yellow"],
+    //color: ["red", "green", "yellow"],
   };
   var myDoughnutChart = new Chart(ctx, {
     type: "doughnut",
     data: data,
+    options: {
+  }
+    
   });
 });
